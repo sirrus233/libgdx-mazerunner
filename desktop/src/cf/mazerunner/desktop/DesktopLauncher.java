a27cbfbd010bb516bd154f7603e518b62ac1900b
Bradley Sherman <sirrus233@gmail.com>
2014-05-28 21:18:03 -0700

    Initial commit

---
+++ desktop/src/cf/mazerunner/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package cf.mazerunner.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import cf.mazerunner.Mazerunner;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Mazerunner(), config);
+	}
+}
